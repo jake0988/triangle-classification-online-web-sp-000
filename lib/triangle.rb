ba3c5a5830f39c87525e0e45497bf412b5e7f3a4
Jake G <jake098877@gmail.com>
2020-05-19 16:02:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,13 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialization(one, two, three)
-  @one = one  
+  @one = one
+  @two = two
+  @three = three
   end
+
+  def length
+    array = [@one, @two, @three]
+  end
+
 end

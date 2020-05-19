bf58f889bc3672dc529c1de1dd941ff4f2852ac0
Jake G <jake098877@gmail.com>
2020-05-19 16:29:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,19 +13,19 @@ attr_accessor :one, :two, :three
       return :equilateral
     end
   end
-
-  def check_length
-    if @one + @two < @three || @one > @two + @three || @one + @three < @two
-      return false
-    elsif @one <= 0 || @two <= 0 || @three <= 0
-      return false
-    end
-  end
-
-  def kindy
-    binding.pry
-    array = [@one, @two, @three]
-    array.each_with_index.sort {|i| i }
-    binding.pry
-  end
+  # 
+  # def check_length
+  #   if @one + @two < @three || @one > @two + @three || @one + @three < @two
+  #     return false
+  #   elsif @one <= 0 || @two <= 0 || @three <= 0
+  #     return false
+  #   end
+  # end
+  #
+  # def kindy
+  #   binding.pry
+  #   array = [@one, @two, @three]
+  #   array.each_with_index.sort {|i| i }
+  #   binding.pry
+  # end
 end
